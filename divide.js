f74c480c5832283df719a66fc2c8f3107be4749c
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-16 00:40:14 +0600

    added divide.js file

---
+++ divide.js
@@ -0,0 +1,12 @@
+function divideMachine(input1, input2) {
+    const divide = input1 / input2;
+    console.log(divide);
+
+    return divide;
+}
+
+divideMachine(10, 5);
+divideMachine(45, 90);
+
+const result = divideMachine(200, 10);
+console.log(result);

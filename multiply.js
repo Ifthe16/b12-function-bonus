428df08d4353aa0d644cb56b25a77fdbd906e3b7
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-16 00:39:40 +0600

    added multiply.js file

---
+++ multiply.js
@@ -0,0 +1,11 @@
+function multiplyMachine(input1, input2) {
+    let multiply = input1 * input2;
+    console.log(multiply);
+
+    return multiply;
+}
+multiplyMachine(6, 12);
+multiplyMachine(10, 20);
+
+const result = multiplyMachine(4, 90);
+console.log(result); 

d7c10e40fae254a85a13f0b90a0ab98c132a8c4a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-16 00:40:41 +0600

    added function.js file

---
+++ function.js
@@ -0,0 +1,24 @@
+console.log("Today we will learning Function");
+
+function hello() {
+    console.log("I am a Function");
+}
+hello();
+
+function moneybag() {
+    var money = 4500;
+    console.log("I have " + money + " Taka");
+}
+moneybag();
+
+
+
+function jogkorarMachine(input1, input2) {
+    let sum = input1 + input2;
+    console.log("Sum is = " + sum);
+    return sum;
+}
+jogkorarMachine(3444, 4111110);
+
+const result = jogkorarMachine(10, 40);
+console.log(result);

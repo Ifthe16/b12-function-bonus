95a7d7bd0d3d28c0bd9053cc470d64a52315e311
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-16 00:39:02 +0600

    added substract.js file

---
+++ subtract.js
@@ -0,0 +1,12 @@
+function subtractMachine(num1, num2) {
+    var sub = num1 - num2;
+    console.log(sub);
+
+    return sub;
+}
+
+subtractMachine(300, 500);
+subtractMachine(-200, 700);
+
+const result = subtractMachine(40, 80);
+console.log(result);

f0a92ccebe37da21fccb2dd4a2a14f4edc1f07e8
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-16 00:38:21 +0600

    added add.js file

---
+++ add.js
@@ -0,0 +1,10 @@
+function addMachine(num1, num2) {
+    let sum = num1 + num2;
+    console.log(sum);
+
+    return sum;
+}
+addMachine(420, 69);
+
+const total = addMachine(33, 77);
+console.log(total);
